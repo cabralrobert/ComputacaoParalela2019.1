8fa4337894d236b0f1821cc1677440a05b6ab5f7
Robert Cabral <robertcabral@alu.ufc.br>
2019-03-10 16:49:39 +0000

    Atividade 01

---
+++ atividades/atividade01/pi.c
@@ -0,0 +1,33 @@
+/*
+ * pi.c
+ *
+ *  Created on: 10 de mar de 2019
+ *      Author: robertcabral
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+float monteCarloPi(int n){
+
+	int i;
+	float acertos = 0, x, y;
+	for(i = 0; i < n; i++){
+		x = ((float)rand()/(float)(RAND_MAX));
+		y = ((float)rand()/(float)(RAND_MAX));
+		
+		if((x * x) + (y * y) < 1)
+			acertos += 1;
+	}
+	return (4 * acertos / n);
+}
+
+int main(int argc, char *argv[]){
+
+	if(argc == 2)
+		printf("pi = %.5f\n", monteCarloPi(atoi(argv[1])));
+	
+	return 0;
+
+}
