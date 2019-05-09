4dc3d6b87ad5de12239d57c3ce8432c0eb1bc0fa
Robert Cabral <robertcabral@alu.ufc.br>
2019-05-09 15:12:27 -0300

    Atividade 04

---
+++ atividades/atividade04/pi.c
@@ -0,0 +1,53 @@
+/************************************************
+ * File Name : pi.c
+ * Creation Date : 22-04-2019
+ * Last Modified : qui 09 mai 2019 15:11:43 -03
+ * Created By : robertcabral@alu.ufc.br
+ * Institution : Universidade Federal do Ceará
+ ************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mpi.h"
+
+float monteCarloPi(int n){
+
+        int i;
+        float acertos = 0, x, y;
+        for(i = 0; i < n; i++){
+                x = ((float)rand()/(float)(RAND_MAX));
+                y = ((float)rand()/(float)(RAND_MAX));
+
+                if((x * x) + (y * y) < 1)
+                        acertos += 1;
+        }
+        return acertos;
+}
+
+int main(int argc, char* argv[]){
+
+    int num_tasks, rank, qtdIterations;
+    float acertos = 0;
+    
+    MPI_Init(&argc, &argv); 
+    MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    srand(time(NULL) + (rank * rank) % RAND_MAX);
+
+    qtdIterations = (atoi(argv[1]) / num_tasks);
+
+    acertos = monteCarloPi(qtdIterations);
+    float valueRes = 0;
+    
+    MPI_Reduce(&acertos, &valueRes, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(rank == 0)
+        printf("%f\n", (4*valueRes/num_tasks)/qtdIterations);
+
+    MPI_Finalize();
+    return 0;
+
+}
+
