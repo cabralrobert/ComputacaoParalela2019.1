f38f49ff203f5d0dba763a331856fa8379237058
Robert Cabral <robertcabral@alu.ufc.br>
2019-06-06 16:47:05 -0300

    Atividade 09

---
+++ atividades/atividade09/src/arquivo.c
@@ -0,0 +1,84 @@
+#include "arquivo.h"
+
+double *initMatrix(int line_size, int column_size){
+    double *matrix;
+    int i, j;
+    matrix = malloc(line_size * column_size * sizeof(double));
+
+    for(i = 0; i < line_size; i++)
+        for(j = 0; j < column_size; j++)
+            MATRIX_POS(matrix, column_size, i, j) = 0;
+
+    return matrix;
+}
+
+double *lerMatriz(FILE *arquivoMatriz, int *linhas, int *colunas){
+    int i, j;
+    double *matrix;
+
+    *linhas = atoi(getNextLine(arquivoMatriz));
+    *colunas = atoi(getNextLine(arquivoMatriz));
+
+    matrix = initMatrix(*linhas, *colunas);
+
+	size_t lineLen = *colunas;
+	char *line = malloc(lineLen);
+    char *element = malloc(lineLen);
+
+    for(i = 0;i < *linhas; i++){
+		line = getNextLine(arquivoMatriz);
+		element = strtok(line, ":");
+		for(j = 0; j < *colunas; j++){
+			if(element)
+				MATRIX_POS(matrix, *colunas, i, j) = atof(element);
+			element = strtok('\0', ":");
+		}
+	}
+    
+//    free(line);
+  //  free(element);
+    
+    return matrix;
+
+}
+
+void imprimirMatriz(double *m, int linhas, int colunas){
+    int i, j;
+    for(i = 0; i < linhas; i++){
+        for(j = 0; j < colunas; j++){
+            printf(" %lf |", MATRIX_POS(m, colunas, i, j));
+        }
+        printf("\n");
+    }
+
+}
+
+void escreverMatriz(char *nomeDoArquivo, double *m, int linhas, int colunas){
+    int i, j;
+    FILE *matrixOut;
+    matrixOut = fopen(nomeDoArquivo, "wt");
+
+    if(matrixOut != NULL){
+    	fprintf(matrixOut, "%d\n", linhas);
+    	fprintf(matrixOut, "%d\n", colunas);
+        for(i = 0; i < linhas; i++){
+            for(j = 0; j < colunas; j++){
+                if(j == colunas - 1)
+                    fprintf(matrixOut, "%.2lf\n", MATRIX_POS(m, colunas, i, j));
+                else
+                    fprintf(matrixOut, "%.2lf:", MATRIX_POS(m, colunas, i, j));
+            }
+        }
+    }
+
+}
+
+void liberarMatriz(double *m){
+    if(m != NULL)
+        free(m);
+}
+
+char* getNextLine(FILE* file){	
+	getline(&lineVet, &tamanho, file);
+	return lineVet;
+}
---
+++ atividades/atividade09/src/multiplicacaoMatrizVetor.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <mpi.h>
+#include "arquivo.h"
+
+
+int main(int argc, char* argv[]){
+
+	if(!argc == 4)
+		return -1;
+
+    FILE* matrix, *vector;
+
+    int linhasMatrix, colunasMatrix;
+    matrix = fopen(argv[MATRIZ], "rt");
+    double* matrixRes = lerMatriz(matrix, &linhasMatrix, &colunasMatrix);
+
+    int linhasVector, colunasVector;
+    vector = fopen(argv[VETOR], "rt");
+    double* vectorRes = lerMatriz(vector, &linhasVector, &colunasVector);
+
+    if(matrixRes == NULL || vectorRes == NULL){
+        printf("Erro ao abrir o arquivo");
+        return -1;
+    }
+
+    // verifica se pode realizar a multiplicação
+	if(colunasMatrix != linhasVector){
+		printf("Numero de colunas diferente do tamanho do vetor.\n");
+		return -1;
+	}
+    
+    // calcula a multiplicação das matrizes
+    int linhasResult = linhasMatrix, colunasResult = colunasVector;
+    double* vetRes = initMatrix(linhasMatrix, colunasVector);
+
+    int i, j, z;
+
+    for(i = 0; i < linhasResult; i++)
+		for(j = 0; j < colunasResult;j++)
+            for(z = 0; z < colunasMatrix; z++)
+    		    MATRIX_POS(vetRes, colunasResult, i, j) +=  MATRIX_POS(matrixRes, colunasMatrix, i, z) * MATRIX_POS(vectorRes, colunasVector, z, j);
+
+    escreverMatriz(argv[RESULTADO], vetRes, linhasResult, colunasResult);
+    imprimirMatriz(vetRes, linhasResult, colunasResult);
+
+    liberarMatriz(matrixRes);
+    liberarMatriz(vectorRes);
+    liberarMatriz(vetRes);
+
+}
